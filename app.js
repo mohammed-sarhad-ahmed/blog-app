d34d69e48876c99eae67c9f744eb6ea7ac50a32d
mohammed-sarhad-ahmed <muhamadsarhad999@gmail.com>
2023-12-26 02:04:28 +0300

    new features

--- app.js
+++ app.js
@@ -1,14 +1,102 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mysql = require("mysql2");
+const { error } = require("console");
+const moment = require("moment");
+const bcrypt = require("bcrypt");
 
 app.set("view engine", "ejs");
 app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.render("blog");
+const con = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "mohammed",
+  password: "A22wweerr@",
+  port: 3307,
+  database: "blog",
 });
 
+con.connect((err) => {
+  if (err) throw err;
+  console.log("the connection to the database was successful");
+});
+
+function handleAddNewPost(req, res) {
+  const dataString = Date(req.body.postDate);
+  req.body.postDate = moment(dataString).format("YYYY-MM-DD HH:mm:ss");
+  const sql = `insert into blogs(id,name,content,postDate) values
+   ('${req.body.id}','${req.body.name}' ,'${req.body.content}','${req.body.postDate}');`;
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(200).end();
+  });
+}
+
+function handleGetAllPosts(req, res) {
+  const sql = "select * from blogs";
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(200).type("html").render("blog", { blogs: result });
+  });
+}
+
+function handleDeleteAllPosts(req, res) {
+  const sql = "delete from blogs";
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(204).end();
+  });
+}
+
+function handleDeleteItem(req, res) {
+  console.log(req.params.id);
+  const sql = ` delete from blogs where id="${req.params.id}" ;`;
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+
+    res.status(204).end();
+  });
+}
+
+app
+  .route("/")
+  .post(handleAddNewPost)
+  .get(handleGetAllPosts)
+  .delete(handleDeleteAllPosts);
+
+app.route("/:id").delete(handleDeleteItem).patch();
+
 app.listen(80, () => {
-  console.log(" started a server at port 80");
+  console.log("the server has started at port 80");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  console.log(req.body.password);
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const sql = `insert into users (username,hashedPassword,email) values('${req.body.username}','${hashedPassword}','${req.body.email}');`;
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+app.post("/login", async (req, res) => {
+  const sql = "select username,hashedPassword from users";
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+    if (bcrypt.compare(req.body.password, result[0].hashedPassword)) {
+      res.redirect("/");
+    }
+  });
 });
--- public/script.js
+++ public/script.js
@@ -2,23 +2,91 @@ const card = document.querySelector(".card");
 const form = document.querySelector(".form");
 const input = document.querySelector("input");
 const textArea = document.querySelector("textarea");
+let deleteBtn = document.querySelector(".deleteAll");
 
-function handleSubmit(e) {
+async function handleSubmit(e) {
+  const id = crypto.randomUUID();
   e.preventDefault();
+  if (card.children.length === 0) {
+    card.insertAdjacentHTML(
+      "beforeend",
+      `<button type="reset" class="btn btn-danger deleteAll">Delete All</button>`
+    );
+    deleteBtn = document.querySelector(".deleteAll");
+    deleteBtn.addEventListener("click", deleteAllHandler);
+  }
   const inputValue = input.value;
   const textAreaValue = textArea.value;
-  const DateTime = new Date().toLocaleString();
-  const cardHtml = `<div class="card-header">
-    ${inputValue}
-  </div>
-  <div class="card-body">
-    <p class="card-text">${textAreaValue}</p>
-    <p class="card-text">${DateTime}</p>
-  </div>`;
-
+  const dateTime = new Date();
+  const cardHtml = `
+  <div class="a-card">
+    <div class="card-header">
+      <span>${inputValue} </span>
+       <div>
+            <i class="fa-regular fa-pen-to-square" data-id=${id} data-type="edit"></i>
+            <i class="fa-solid fa-delete-left" data-id=${id} data-type="delete"></i>
+          </div>
+    </div>
+    <div class="card-body">
+      <p class="card-text">${textAreaValue}</p>
+      <p class="card-text">${dateTime.toLocaleString()}</p>
+    </div>
+  </div>  
+    `;
+  const newCard = {
+    id,
+    name: inputValue,
+    content: textAreaValue,
+    postDate: dateTime,
+  };
   input.value = "";
   textArea.value = "";
   card.insertAdjacentHTML("afterbegin", cardHtml);
+
+  await fetch("http://localhost", {
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(newCard),
+  });
+}
+
+async function deleteAllHandler(e) {
+  card.innerHTML = "";
+  await fetch("/", {
+    method: "delete",
+  });
+}
+
+async function handleDeleteItem(id, target) {
+  target.remove();
+  await fetch(`/${id}`, {
+    method: "delete",
+  });
+}
+
+function handleEditItem(id, target) {
+  const tragetName = target.querySelector("[data-type='username']");
+  const tragetContent = target.querySelector("[data-type='content']");
+  const tragetDate = target.querySelector("[data-type='username']");
+
+  const html = `
+  
+  
+  `;
 }
 
 form.addEventListener("submit", handleSubmit);
+deleteBtn?.addEventListener("click", deleteAllHandler);
+card.addEventListener("click", (e) => {
+  if (e.target.dataset.type !== "delete" && e.target.dataset.type !== "edit")
+    return;
+  const target = document
+    .querySelector(`[data-id="${e.target.dataset.id}"]`)
+    .closest(".a-card");
+  const id = e.target.dataset.id;
+  if (e.target.dataset.type === "delete") handleDeleteItem(id, target);
+
+  if (e.target.dataset.type === "edit") handleEditItem(id, target);
+});
