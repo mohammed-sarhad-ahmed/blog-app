ba6ed296c0f319581e8e2fff308992fab2a2cfc1
mohammed-sarhad-ahmed <muhamadsarhad999@gmail.com>
2023-12-26 14:22:18 +0300

    new features

--- app.js
+++ app.js
@@ -5,12 +5,31 @@ const mysql = require("mysql2");
 const { error } = require("console");
 const moment = require("moment");
 const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 app.set("view engine", "ejs");
 app.use("/static", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "pure platform internship 2023-2024",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
+app.use((req, res, next) => {
+  if (
+    !req.session.username &&
+    req.path !== "/login" &&
+    req.path !== "/register"
+  ) {
+    res.redirect("login");
+  } else {
+    next();
+  }
+});
 const con = mysql.createConnection({
   host: "127.0.0.1",
   user: "mohammed",
@@ -27,8 +46,8 @@ con.connect((err) => {
 function handleAddNewPost(req, res) {
   const dataString = Date(req.body.postDate);
   req.body.postDate = moment(dataString).format("YYYY-MM-DD HH:mm:ss");
-  const sql = `insert into blogs(id,name,content,postDate) values
-   ('${req.body.id}','${req.body.name}' ,'${req.body.content}','${req.body.postDate}');`;
+  const sql = `insert into blogs(id,username,content,postDate) values
+   ('${req.body.id}','${req.body.username}' ,'${req.body.content}','${req.body.postDate}');`;
   con.query(sql, (err, result) => {
     if (err) throw err;
     res.status(200).end();
@@ -36,15 +55,24 @@ function handleAddNewPost(req, res) {
 }
 
 function handleGetAllPosts(req, res) {
-  const sql = "select * from blogs";
+  const sql = `select blogs.id,blogs.username,blogs.content,blogs.postDate,
+      sum (
+        case when type="up" then 1
+        else -1
+        end
+      ) as numberOfLikes 
+      from votes join blogs on blog_id=blogs.id group by blogs.id`;
+
   con.query(sql, (err, result) => {
-    if (err) throw err;
-    res.status(200).type("html").render("blog", { blogs: result });
+    res.status(200).type("html").render("blog", {
+      blogs: result,
+      username: req.session.username,
+    });
   });
 }
 
 function handleDeleteAllPosts(req, res) {
-  const sql = "delete from blogs";
+  const sql = `delete from blogs where username='${req.session.username}'`;
   con.query(sql, (err, result) => {
     if (err) throw err;
     res.status(204).end();
@@ -60,6 +88,14 @@ function handleDeleteItem(req, res) {
     res.status(204).end();
   });
 }
+function handleLikes(req, res) {
+  const sql = `insert into votes (id,username,blog_id,type) values
+   ('${req.body.id}','${req.body.username}','${req.body.blogId}','${req.body.type}')`;
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(200).end();
+  });
+}
 
 app
   .route("/")
@@ -67,36 +103,58 @@ app
   .get(handleGetAllPosts)
   .delete(handleDeleteAllPosts);
 
-app.route("/:id").delete(handleDeleteItem).patch();
-
 app.listen(80, () => {
   console.log("the server has started at port 80");
 });
 
 app.get("/login", (req, res) => {
-  res.render("login");
+  if (req.session.username) return res.redirect("/");
+  res.render("login", { message: null });
 });
 
 app.get("/register", (req, res) => {
+  if (req.session.username) return res.redirect("/");
   res.render("register");
 });
 
 app.post("/register", async (req, res) => {
   console.log(req.body.password);
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  const sql = `insert into users (username,hashedPassword,email) values('${req.body.username}','${hashedPassword}','${req.body.email}');`;
+  const sql = `insert into users (username,hashedPassword,email) 
+  values('${req.body.username}','${hashedPassword}','${req.body.email}');`;
+
   con.query(sql, (err, result) => {
     if (err) throw err;
+    req.session.username = req.body.username;
     res.redirect("/");
   });
 });
 
-app.post("/login", async (req, res) => {
-  const sql = "select username,hashedPassword from users";
-  con.query(sql, (err, result) => {
+app.post("/login", (req, res) => {
+  const sql = `select username,hashedPassword from users where username='${req.body.username}';`;
+  con.query(sql, async (err, result) => {
     if (err) throw err;
-    if (bcrypt.compare(req.body.password, result[0].hashedPassword)) {
+    if (result.length === 0)
+      return res.render("login", {
+        message: "username or password was incorrect",
+      });
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      result[0].hashedPassword
+    );
+    if (isPasswordCorrect) {
+      req.session.username = req.body.username;
       res.redirect("/");
+    } else {
+      res.render("login", {
+        message: "username or password was incorrect",
+      });
     }
   });
 });
+
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/login");
+});
+app.route("/:id").post(handleLikes).delete(handleDeleteItem).patch();
--- public/script.js
+++ public/script.js
@@ -3,14 +3,17 @@ const form = document.querySelector(".form");
 const input = document.querySelector("input");
 const textArea = document.querySelector("textarea");
 let deleteBtn = document.querySelector(".deleteAll");
-
+const logout = document.querySelector("#logout");
 async function handleSubmit(e) {
   const id = crypto.randomUUID();
   e.preventDefault();
-  if (card.children.length === 0) {
+  const numOfCurrentUSerPosts = document.querySelectorAll(
+    `[data-username=${input.value}]`
+  ).length;
+  if (numOfCurrentUSerPosts === 0) {
     card.insertAdjacentHTML(
       "beforeend",
-      `<button type="reset" class="btn btn-danger deleteAll">Delete All</button>`
+      `<button type="reset" class="btn btn-danger deleteAll">Delete All Your posts</button>`
     );
     deleteBtn = document.querySelector(".deleteAll");
     deleteBtn.addEventListener("click", deleteAllHandler);
@@ -19,9 +22,15 @@ async function handleSubmit(e) {
   const textAreaValue = textArea.value;
   const dateTime = new Date();
   const cardHtml = `
-  <div class="a-card">
+    <div class="the-card"> 
+     <div class="likes">
+          <i class="fa-solid fa-up-long" data-type="up" data-id=${id}></i>
+          <span class="numOflikes">0</span>
+          <i class="fa-solid fa-down-long" data-type="down" data-id=${id}></i>
+        </div>
+      <div class="a-card">
     <div class="card-header">
-      <span>${inputValue} </span>
+      <span data-type="username" data-username='${inputValue}' >YOU</span>
        <div>
             <i class="fa-regular fa-pen-to-square" data-id=${id} data-type="edit"></i>
             <i class="fa-solid fa-delete-left" data-id=${id} data-type="delete"></i>
@@ -32,18 +41,18 @@ async function handleSubmit(e) {
       <p class="card-text">${dateTime.toLocaleString()}</p>
     </div>
   </div>  
+
+    </div>
     `;
   const newCard = {
     id,
-    name: inputValue,
+    username: inputValue,
     content: textAreaValue,
     postDate: dateTime,
   };
-  input.value = "";
   textArea.value = "";
   card.insertAdjacentHTML("afterbegin", cardHtml);
-
-  await fetch("http://localhost", {
+  await fetch("/", {
     method: "post",
     headers: {
       "content-type": "application/json",
@@ -53,7 +62,11 @@ async function handleSubmit(e) {
 }
 
 async function deleteAllHandler(e) {
-  card.innerHTML = "";
+  const yourPosts = card.querySelectorAll(`[data-username=${input.value}]`);
+
+  yourPosts.forEach((post) => post.closest(".the-card").remove());
+  e.target.remove();
+
   await fetch("/", {
     method: "delete",
   });
@@ -61,32 +74,60 @@ async function deleteAllHandler(e) {
 
 async function handleDeleteItem(id, target) {
   target.remove();
+  const numOfCurrentUSerPosts = card.querySelector(
+    `[data-username='${input.value}']`
+  );
+  if (!numOfCurrentUSerPosts) card.querySelector(".deleteAll").remove();
   await fetch(`/${id}`, {
     method: "delete",
   });
 }
+// TODO
+function handleEditItem(id, target) {}
 
-function handleEditItem(id, target) {
-  const tragetName = target.querySelector("[data-type='username']");
-  const tragetContent = target.querySelector("[data-type='content']");
-  const tragetDate = target.querySelector("[data-type='username']");
-
-  const html = `
-  
-  
-  `;
+function handleLikes(id, blogId, type, target) {
+  const el = target.querySelector(".numOflikes");
+  if (type === "up") {
+    el.innerText = Number(el.innerText) + 1;
+    target.querySelector("[data-type='up']").style.color = "red";
+    target.querySelector("[data-type='down']").style.color = "white";
+  } else if (type === "down") {
+    el.innerText = Number(el.innerText) - 1;
+    target.querySelector("[data-type='down']").style.color = "red";
+    target.querySelector("[data-type='up']").style.color = "white";
+  }
+  fetch(`/${id}`, {
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      id: crypto.randomUUID(),
+      type,
+      blogId,
+      username: input.value,
+    }),
+  });
 }
 
 form.addEventListener("submit", handleSubmit);
 deleteBtn?.addEventListener("click", deleteAllHandler);
 card.addEventListener("click", (e) => {
-  if (e.target.dataset.type !== "delete" && e.target.dataset.type !== "edit")
+  if (
+    e.target.dataset.type !== "delete" &&
+    e.target.dataset.type !== "edit" &&
+    e.target.dataset.type !== "up" &&
+    e.target.dataset.type !== "down"
+  )
     return;
   const target = document
     .querySelector(`[data-id="${e.target.dataset.id}"]`)
-    .closest(".a-card");
+    .closest(".the-card");
   const id = e.target.dataset.id;
   if (e.target.dataset.type === "delete") handleDeleteItem(id, target);
 
   if (e.target.dataset.type === "edit") handleEditItem(id, target);
+
+  if (e.target.dataset.type === "up" || e.target.dataset.type === "down")
+    handleLikes(id, e.target.dataset.id, e.target.dataset.type, target);
 });
